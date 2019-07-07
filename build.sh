4084ff4cd9a5f5847aaa7ce5b122d0ce092ced0a
Lukman Shaikh <shaikhlukman01@gmail.com>
2019-07-07 19:10:44 +0530

    Fix build
    Signed-off-by: Lukman Shaikh <shaikhlukman01@gmail.com>

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 # Main Environment
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=$KERNEL_DIR/AnyKernel3
+ZIP_DIR=$KERNEL_DIR/AnyKernel2
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
 CONFIG=land_defconfig
 CORES=$(grep -c ^processor /proc/cpuinfo)
 THREAD="-j$CORES"
-CROSS_COMPILE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
-CLANG_TRIPLE+="$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-"
 
 echo -e "\n(i) Cloning toolcahins if folder not exist..."
-git clone https://github.com/raza231198/aarch64-linux-android-4.9
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 
 echo -e "\n(i) Cloning clang if folder not exist..."
 git clone https://github.com/lukmanshaikh/aosp-clang clang
@@ -18,9 +16,10 @@ git clone https://github.com/lukmanshaikh/aosp-clang clang
 # Export
 export ARCH=arm64
 export SUBARCH=arm64
+PATH="$PWD/clang/bin:$PWD/aarch64-linux-android-4.9/bin/:${PATH}"
 export CC=clang
-export CLANG_TRIPLE
-export CROSS_COMPILE
+CLANG_TRIPLE=aarch64-linux-gnu-
+CROSS_COMPILE=aarch64-linux-android-
 export KBUILD_BUILD_USER=luqman
 export KBUILD_BUILD_HOST=unique
 make  O=out $CONFIG $THREAD
